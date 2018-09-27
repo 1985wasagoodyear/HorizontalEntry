79412a04015916ba5359cf378b39a3e72a60ad5b
Kevin Yu <kevin.yu@mobileappscompany.com>
2018-09-27 16:24:48 -0400

    first commit

---
+++ HETableView/HETableView.h
@@ -0,0 +1,19 @@
+//
+//  HETableView.h
+//  HETableView
+//
+//  Created by Kevin Yu on 9/27/18.
+//  Copyright © 2018 Kevin Yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HETableView.
+FOUNDATION_EXPORT double HETableViewVersionNumber;
+
+//! Project version string for HETableView.
+FOUNDATION_EXPORT const unsigned char HETableViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HETableView/PublicHeader.h>
+
+
---
+++ HorizontalEntry/SampleViewController.swift
@@ -0,0 +1,50 @@
+//
+//  SampleViewController.swift
+//  HorizontalEntry
+//
+//  Created by Kevin Yu on 9/27/18.
+//  Copyright © 2018 Kevin Yu. All rights reserved.
+//
+
+import UIKit
+import HETableView
+
+class SampleViewController: UIViewController {
+
+    let cellIdentifier = "cell"
+    
+    @IBOutlet weak var tableView: HETableView!
+    
+    let sampleData = ["Dog", "Cat", "Rabbit", "Bread", "Kitten", "Horse"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+    }
+
+
+}
+
+extension SampleViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sampleData.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        cell.textLabel?.text = sampleData[indexPath.row];
+        return cell
+    }
+}
+
+extension SampleViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        HETableView.performAnimation(tableView, willDisplay: cell, forRowAt: indexPath)
+    }
+
+}
